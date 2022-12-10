f0348eecf7340e6442dba73396435365d06f6e5b
webittoday <119814564+webittoday@users.noreply.github.com>
2022-12-10 23:57:05 +0200

    Update move_items.sh

--- move_items.sh
+++ move_items.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # move temps  
-yes | cp -rf /templates/temps/* /home/ec2-user/app/temps/
+yes | cp -rf ./temps/* /home/ec2-user/app/temps/
 
 # move maps
-yes | cp -rf /templates/maps/* /home/ec2-user/app/maps/
+yes | cp -rf ./maps/* /home/ec2-user/app/maps/
