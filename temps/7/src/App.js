3a79691c4c15ee1a500e4082b8204937502fe04d
Alon Kigler <kiglerr@gmail.com>
2022-12-13 15:16:49 +0200

    fix45

---
+++ temps/7/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
