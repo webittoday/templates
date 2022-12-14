99a34efa9dd2f994dfb2b4e7abbdd58ab6120be2
Alon Kigler <kiglerr@gmail.com>
2022-12-14 18:19:53 +0200

    7

--- maps/a7.py
+++ maps/a7.py
@@ -1,12 +1,45 @@
 questions = [
 
-    {"QUESTION":" כותרת ", "VARNAME": "title", "type": "textarea"}
-]
+    {"QUESTION": "כותרת לדף אודות", "VARNAME": "header", "type": "textarea"},
+    {"QUESTION": "תוכן לדף אודות", "VARNAME": "about", "type": "textarea"},
+    {"QUESTION": "תוכן לדף אודות שני", "VARNAME": "about2", "type": "textarea"},
+    {"QUESTION": "תמונות לגלריה", "VARNAME": "galleryurl",
+        "type": "image", "limit": 6, "dimension": "360x230"},
+    {"QUESTION": "לוגו", "VARNAME": "logourl",
+        "type": "image", "limit": 1, "dimension": "150x150"},
 
+
+    {"QUESTION": "כותרת לקטע מידע ראשון", "VARNAME": "sec1", "type": "text"},
+    {"QUESTION": "קטע מידע ראשון", "VARNAME": "secT1", "type": "textarea"},
+    {"QUESTION": "כותרת לקטע מידע שני", "VARNAME": "sec2", "type": "text"},
+    {"QUESTION": "קטע מידע שני", "VARNAME": "secT2", "type": "textarea"},
+    {"QUESTION": "כותרת לקטע מידע שלישי", "VARNAME": "sec3", "type": "text"},
+    {"QUESTION": "קטע מידע שלישי", "VARNAME": "secT3", "type": "textarea"},
+
+
+
+]
 
 
 replacements = [
 
-    {"KEYWORD": "INSERTHEADER", "VARNAME": "title", "SRC": "src/New/components/HeaderSection.js"},
+    {"KEYWORD": "INSERTLOGO", "VARNAME": "logourl", "SRC": "public/index.html"},
+    {"KEYWORD": "INSERTNAME", "VARNAME": "business_name", "SRC": "public/index.html"},
+
+    {"KEYWORD": "INSERTNAME", "VARNAME": "business_name", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTSLOGAN", "VARNAME": "slogan", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTLOGO", "VARNAME": "logourl", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTHEADER", "VARNAME": "header", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTABOUT", "VARNAME": "about", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTABOUT2", "VARNAME": "about2", "SRC": "src/App.js"},
+
+    {"KEYWORD": "INSERTSEC1", "VARNAME": "sec1", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTSECT1", "VARNAME": "secT1", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTSEC2", "VARNAME": "sec2", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTSECT2", "VARNAME": "secT2", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTSEC3", "VARNAME": "sec3", "SRC": "src/App.js"},
+    {"KEYWORD": "INSERTSECT3", "VARNAME": "secT3", "SRC": "src/App.js"},
+
+    {"KEYWORD": "GALLERY", "VARNAME": "galleryurl", "SRC": "src/App.js"},
 
 ]
--- temps/7/src/components/Navbar.js
+++ temps/7/src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import logo from "../assets/logo-light.png"
-import { GiHamburgerMenu } from "react-icons/gi"
+import React, { useEffect, useState } from "react";
+import logo from "../assets/logo-light.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
   const [scroll, setScroll] = useState(false);
@@ -10,25 +10,35 @@ export default function Navbar() {
     });
     return () => {
       setScroll({});
-    }
+    };
   }, []);
   return (
-    <header className={`header ${scroll ? 'is-fixed' : ''}`}>
-      <nav >
+    <header className={`header ${scroll ? "is-fixed" : ""}`}>
+      <nav>
         <input type="checkbox" id="check" />
-        <label htmlFor="check" className='checkBtn '>
-          <GiHamburgerMenu className='nav-icon' />
+        <label htmlFor="check" className="checkBtn ">
+          <GiHamburgerMenu className="nav-icon" />
         </label>
-        <label className='logo'><img src={logo} alt="" /></label>
+        <label className="logo">
+          <img src={logo} alt="" />
+        </label>
         <ul>
-          <li><a href="#" className='active'>Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#">Gallery</a></li>
-          <li><a href="#">Team</a></li>
-
+          <li>
+            <a href="#" className="active">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#HomeSectionTwo">About</a>
+          </li>
+          <li>
+            <a href="#HomeContact">Contact</a>
+          </li>
+          <li>
+            <a href="#HomeGallery">Gallery</a>
+          </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
